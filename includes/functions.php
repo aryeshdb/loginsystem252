65379800f7ba05304d62444671668e1701b5e4f6
aryeshdb <aryeshdb@iitk.ac.in>
2018-11-10 15:49:15 +0530

    Updated1

--- includes/functions.php
+++ includes/functions.php
@@ -45,75 +45,83 @@ function sec_session_start() {
 
 function login($email, $password, $mysqli) {
     echo "{$email}";
+    $sql= "SELECT * FROM members WHERE email='test@example.com'";
+     $select=$mysqli->query($sql);
+  if(mysqli_num_rows($select)>0)
+  {
+    echo "found  ";
+  }
+    echo " not found  ";
+  
     // Using prepared statements means that SQL injection is not possible. 
-    if ($stmt = $mysqli->prepare("SELECT id, username, password, salt 
-				  FROM members 
-                                  WHERE email = ? LIMIT 1")) {
-        $stmt->bind_param('s', $email);  // Bind "$email" to parameter.
-        $stmt->execute();    // Execute the prepared query.
-        $stmt->store_result();
+    // if ($stmt = $mysqli->prepare("SELECT id, username, password, salt 
+				//   FROM members 
+    //                               WHERE email = ? LIMIT 1")) {
+    //     $stmt->bind_param('s', $email);  // Bind "$email" to parameter.
+    //     $stmt->execute();    // Execute the prepared query.
+    //     $stmt->store_result();
 
-        // get variables from result.
-        $stmt->bind_result($user_id, $username, $db_password, $salt);
-        $stmt->fetch();
+    //     // get variables from result.
+    //     $stmt->bind_result($user_id, $username, $db_password, $salt);
+    //     $stmt->fetch();
 
-        // hash the password with the unique salt.
-        $password = hash('sha512', $password . $salt);
-        echo "{$email}";
-        if ($stmt->num_rows == 1) {
-            echo "{$email}";
-            // If the user exists we check if the account is locked
-            // from too many login attempts 
-            if (checkbrute($user_id, $mysqli) == true) {
-                // Account is locked 
-                echo "in check brute  ";
-                // Send an email to user saying their account is locked 
-                return false;
-            } else {
-                echo "checking password";
+    //     // hash the password with the unique salt.
+    //     $password = hash('sha512', $password . $salt);
+    //     echo "{$email}";
+    //     if ($stmt->num_rows == 1) {
+    //         echo "{$email}";
+    //         // If the user exists we check if the account is locked
+    //         // from too many login attempts 
+    //         if (checkbrute($user_id, $mysqli) == true) {
+    //             // Account is locked 
+    //             echo "in check brute  ";
+    //             // Send an email to user saying their account is locked 
+    //             return false;
+    //         } else {
+    //             echo "checking password";
     
-                // Check if the password in the database matches 
-                // the password the user submitted.
-                if ($db_password == $password) {
-                    // Password is correct!
-                    // Get the user-agent string of the user.
-                    echo "password is correct";
-                    $user_browser = $_SERVER['HTTP_USER_AGENT'];
+    //             // Check if the password in the database matches 
+    //             // the password the user submitted.
+    //             if ($db_password == $password) {
+    //                 // Password is correct!
+    //                 // Get the user-agent string of the user.
+    //                 echo "password is correct";
+    //                 $user_browser = $_SERVER['HTTP_USER_AGENT'];
 
-                    // XSS protection as we might print this value
-                    $user_id = preg_replace("/[^0-9]+/", "", $user_id);
-                    $_SESSION['user_id'] = $user_id;
+    //                 // XSS protection as we might print this value
+    //                 $user_id = preg_replace("/[^0-9]+/", "", $user_id);
+    //                 $_SESSION['user_id'] = $user_id;
 
-                    // XSS protection as we might print this value
-                    $username = preg_replace("/[^a-zA-Z0-9_\-]+/", "", $username);
+    //                 // XSS protection as we might print this value
+    //                 $username = preg_replace("/[^a-zA-Z0-9_\-]+/", "", $username);
 
-                    $_SESSION['username'] = $username;
-                    $_SESSION['login_string'] = hash('sha512', $password . $user_browser);
+    //                 $_SESSION['username'] = $username;
+    //                 $_SESSION['login_string'] = hash('sha512', $password . $user_browser);
 
-                    // Login successful. 
-                    return true;
-                } else {
-                    // Password is not correct 
-                    // We record this attempt in the database 
-                    $now = time();
-                    if (!$mysqli->query("INSERT INTO login_attempts(user_id, time) 
-                                    VALUES ('$user_id', '$now')")) {
-                        header("Location: ../error.php?err=Database error: login_attempts");
-                        exit();
-                    }
+    //                 // Login successful. 
+    //                 return true;
+    //             } else {
+    //                 // Password is not correct 
+    //                 // We record this attempt in the database 
+    //                 $now = time();
+    //                 if (!$mysqli->query("INSERT INTO login_attempts(user_id, time) 
+    //                                 VALUES ('$user_id', '$now')")) {
+    //                     header("Location: ../error.php?err=Database error: login_attempts");
+    //                     exit();
+    //                 }
 
-                    return false;
-                }
-            }
-        } else {
-            // No user exists. 
-            return false;
-        }
-    } else {
-        // Could not create a prepared statement
-        header("Location: ../error.php?err=Database error: cannot prepare statement");
-        exit();
-    }
+    //                 return false;
+    //             }
+    //         }
+    //     } else {
+    //         // No user exists. 
+    //         return false;
+    //     }
+    // } else {
+    //     // Could not create a prepared statement
+    //     header("Location: ../error.php?err=Database error: cannot prepare statement");
+    //     exit();
+    // }
 }
 
 function checkbrute($user_id, $mysqli) {
